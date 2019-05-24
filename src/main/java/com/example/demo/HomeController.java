40d25a65f6fa5eea2605f6bc3fe9e3f25c068cae
xiong.bob@gmail.com <xiong.bob@gmail.com>
2019-05-24 12:08:28 -0400

    whatever-commit-203

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,19 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @Controller
 public class HomeController {
-    @RequestMapping("/songform")
+    @GetMapping("/songform")
     public String loadFormPage(Model model) {
         model.addAttribute("song", new Song());
         return "songform";
     }
 
-    @RequestMapping("/songform")
+    @PostMapping("/songform")
     public String loadFormPage(@ModelAttribute Song song, Model model){
         model.addAttribute("Song", song);
         return "confirmsong";
